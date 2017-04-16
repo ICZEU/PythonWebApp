f2193618c8fa126a7edd3d857b7eb616996ffa1f
Peter Doering <Peter Doering>
2017-04-16 21:11:52 +0200

    Enabled WSGI hosting.

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from os import environ
 from sermonapp import app
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5000'))
+    except ValueError:
+        PORT = 5000
+    app.run(HOST, PORT, debug = True)
